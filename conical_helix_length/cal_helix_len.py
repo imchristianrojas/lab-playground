ce729bc756f96a93d8c7b19a183e4cb9a7f85553
Christian <crojas0315@gmail.com>
2025-12-03 18:18:16 -0800

    added file

---
+++ conical_helix_length/cal_helix_len.py
@@ -0,0 +1,18 @@
+import math as m
+import numpy as np
+
+
+
+def calculate_len(height: float,radius: float = 1.0) -> float:
+
+
+    return 0.0
+
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
